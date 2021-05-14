7d0e23c206bb58796ab45d388add1e67e573504d
xblanc33 <blancxav@gmail.com>
2021-05-14 13:39:08 +0200

    add api config

---
+++ api/src/_infra/config.ts
@@ -0,0 +1,19 @@
+const config = {
+    port: 80,
+    host: "api",
+    website: {
+        host: "website",
+        port: 80,
+        
+    },
+    session: {
+        host: "session",
+        port: 80
+    },
+    account: {
+        host: "account",
+        port: 80
+    }
+}
+
+export default config 
