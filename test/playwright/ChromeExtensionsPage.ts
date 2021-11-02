d9f325df20fde78c4f1932c43204c31613010ffe
xblanc33 <blancxav@gmail.com>
2021-11-02 23:53:02 +0100

    test Playwright in github

--- test/playwright/ChromeExtensionsPage.ts
+++ test/playwright/ChromeExtensionsPage.ts
@@ -16,20 +16,36 @@ export default class ChromeExtensionsPage {
     async getNumberOfExtensions() {
         await this._page.waitForSelector(EXTENSIONS_SELECTOR);
         return await this._page.evaluate( () => {
-            let manager = document.body.querySelector('extensions-manager').shadowRoot;
-            let itemList = manager.getElementById('items-list').shadowRoot;
-            let extensionItem = itemList.querySelectorAll('.items-container extensions-item');
-            return extensionItem.length;
+            const extensionManager = document.body.querySelector('extensions-manager')
+            if (extensionManager && extensionManager.shadowRoot) {
+                let manager = extensionManager.shadowRoot;
+                const itemList = manager.getElementById('items-list');
+                if (itemList && itemList.shadowRoot) {
+                    let extensionItem = itemList.shadowRoot.querySelectorAll('.items-container extensions-item');
+                    if (extensionItem) {
+                        return extensionItem.length;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    return 0;
+                }
+            } else {
+                return 0;
+            }
         });
     }
 
-    async getAIFEXExtensionId() {
-        
+    async getAIFEXExtensionId() : Promise<string|undefined> {
         await this._page.waitForSelector(EXTENSIONS_SELECTOR);
         return await this._page.evaluate( () => {
-            let manager = document.body.querySelector('extensions-manager').shadowRoot;
-            let itemList = manager.getElementById('items-list').shadowRoot;
-            return itemList.querySelectorAll('.items-container extensions-item')[0].id;
+            let manager = document.body.querySelector('extensions-manager');
+            if (manager && manager.shadowRoot) {
+                let itemList = manager.shadowRoot.getElementById('items-list');
+                if (itemList && itemList.shadowRoot) {
+                    return itemList.shadowRoot.querySelectorAll('.items-container extensions-item')[0].id;
+                }
+            }
         })
     }
 
--- test/playwright/Test.ts
+++ test/playwright/Test.ts
@@ -1,5 +1,5 @@
 import chai from "chai";
-import { chromium } from 'playwright';
+import { BrowserContext, chromium } from 'playwright';
 import BrowserExtensionPage from './BrowserExtensionPage';
 import ChromeExtensionsPage from './ChromeExtensionsPage';
 const expect = chai.expect;
@@ -8,23 +8,21 @@ import DashboardHomePage from "./DashboardHomePage";
 import DashboardSignUpPage from "./DashboardSignUpPage";
 import DashboardSignInPage from "./DashboardSignInPage";
 import DashboardAccountPage from "./DashboardAccountPage";
+import path from "path";
 
 
 describe("Playwright", () => {
 
-    let browser;
 
-    before("creating browser", async () => {
-        const path = require("path")
-        let PATH_TO_EXTENSION;
-        let DASHBOARD_HOST;
+    let browser : BrowserContext;
+    let PATH_TO_EXTENSION = path.join(__dirname, '..', '..', 'browser-extension', 'dist', "chrome");
+    let DASHBOARD_URL = 'http://localhost/';
 
+
+    before("creating browser", async () => {        
         if (process.env.NODE_ENV === 'github') {
-            DASHBOARD_HOST = 'dashboard';
+            DASHBOARD_URL = 'http://dashboard/';
             PATH_TO_EXTENSION = '/browser-extension/dist/chrome';
-        } else {
-            DASHBOARD_HOST = 'localhost';
-            PATH_TO_EXTENSION = path.join(__dirname, '..', '..', 'browser-extension', 'dist', "chrome");
         }
 
         console.log(PATH_TO_EXTENSION);
@@ -62,28 +60,28 @@ describe("Playwright", () => {
 
     it("should go to the dashboard and try for free", async () => {
         const page = await browser.newPage();
-        const dhp = new DashboardHomePage(page);
+        const dhp = new DashboardHomePage(page, DASHBOARD_URL);
         await dhp.goto();
         await dhp.tryForFree();
     })
 
     it("should create a new user", async () => {
         const page = await browser.newPage();
-        const dsup = new DashboardSignUpPage(page);
+        const dsup = new DashboardSignUpPage(page, DASHBOARD_URL);
         await dsup.goto();
         await dsup.signup('playwright', 'playwright', 'playwright');
     })
 
     it("should signin", async () => {
         const page = await browser.newPage();
-        const dsip = new DashboardSignInPage(page);
+        const dsip = new DashboardSignInPage(page, DASHBOARD_URL);
         await dsip.goto();
         await dsip.signin('playwright', 'playwright');
     })
 
     it("should create a new sesion", async () => {
         const page = await browser.newPage();
-        const dap = new DashboardAccountPage(page);
+        const dap = new DashboardAccountPage(page, DASHBOARD_URL);
         await dap.goto();
         await dap.startNewSession();
     })
