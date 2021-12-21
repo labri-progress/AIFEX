446af0e8ca7f4f6790371268005ccc30b060d18e
Xavier Blanc <xavier.blanc@u-bordeaux.fr>
2021-12-21 17:05:07 +0100

    AifexService

--- browser-script/src/domain/BrowserScript.ts
+++ browser-script/src/domain/BrowserScript.ts
@@ -2,24 +2,88 @@ import Action from "./Action";
 import AifexService from "./AifexService";
 import EventListener from "./EventListener";
 import RuleService from "./RuleService";
+import Token from "./Token";
 
 
 export default class TabScript {
 
-    private _ruleService : RuleService;
-    private _eventListener : EventListener;
-    private _aifexService: AifexService;
+    private _serverURL: string | undefined;
+	private _sessionId: string | undefined;
+	private _webSiteId: string | undefined;
+	private _token: Token | undefined;
+    private _ruleService : RuleService | undefined;
+    private _eventListener : EventListener | undefined;
+    private _aifexService : AifexService | undefined;
+
     
-    constructor(aifexService: AifexService) {
-        this._ruleService = new RuleService();
-        this._aifexService = aifexService,
-        this._eventListener = new EventListener(this._ruleService);
-        this._eventListener.listen();
-        this._eventListener.onNewUserAction(this.onNewAction.bind(this));
+    constructor(connectionURL: string, token: Token | undefined, aifexService: AifexService) {
+        try {
+			const CONNECTION_URL = new URL(connectionURL);
+			let sessionId = CONNECTION_URL.searchParams.get('sessionId');
+			if (sessionId) {
+				this._sessionId = sessionId;
+			}
+			this._serverURL = CONNECTION_URL.origin;
+			this._token = token;
+            this._ruleService = new RuleService();
+            this._eventListener = new EventListener(this._ruleService);
+            this._aifexService = aifexService;
+		} catch (e) {
+			console.error('wrong connectionURL !!! Won\'t listen to any action');
+		}
+        
     }
 
-    onNewAction(action: Action) {
-        
+    synchronizeWithBackground() : Promise<void> {
+        return this._backgroundService.getState()
+        .then(state => {
+            const rules = state.webSite.mappingList.map((ru : any) => this._ruleService.createRule(ru));
+            this._ruleService.loadRules(rules);
+            this._ruleService.mapRulesToElements();
+            if (state.isActive) {
+                this.explorationStarted();
+            } else {
+                this._highlighter.hide();
+            }
+        })
+    }
+
+    processNewAction(prefix: string, suffix?: string): Promise<void> {
+        if (this._isRecording && this._exploration) {
+            this._exploration.addAction(prefix, suffix);
+            this._commentsUp = [];
+
+            const promises = [
+                this.fetchComments(),
+                this.evaluateExploration(),
+                this.fetchProbabilityMap()
+            ];
+
+            if (this._recordActionByAction) {
+                if (!this._serverURL ||  !this._sessionId) {
+                    throw new Error("Not connected to a session")
+                }
+                if (this._exploration.explorationNumber === undefined) {
+                    throw new Error("The exploration has not been correctly started")
+                }
+                const actionList = this._exploration.actions;
+                const lastAction = actionList[actionList.length-1];
+                const pushActionListPromise = this._aifexService.pushActionOrCommentList(
+                    this._serverURL, 
+                    this._sessionId, 
+                    this._exploration.explorationNumber, 
+                    [lastAction])
+
+                promises.push(pushActionListPromise);
+            }
+            return Promise.all(promises)
+                .then(() => {
+                    this.refreshPopup();
+                })
+
+        } else {
+            return Promise.resolve();
+        }
     }
 
 }
--- browser-script/src/domain/Session.ts
+++ browser-script/src/domain/Session.ts
@@ -5,21 +5,24 @@ export default class Session {
     readonly id : string;
     readonly webSiteId : string;
     readonly baseURL: string | undefined;
+    readonly name : string;
+    readonly description : string | undefined;
     readonly overlayType: OverlayType;
     readonly recordingMode: RecordingMode;
 
-    constructor(id: string, webSiteId: string, overlayType: OverlayType, recordingMode: RecordingMode, baseURL?:string) {
+    constructor(id: string, webSiteId: string, baseURL:string | undefined, name : string, description : string, overlayType: OverlayType, recordingMode: RecordingMode) {
         if (id === null || id === undefined) {
             throw new Error('cannot create Session without id');
         }
-        this.overlayType = overlayType;
-        this.id = id;
-
         if (webSiteId === null || webSiteId === undefined) {
             throw new Error('cannot create Session without webSiteId');
         }
+        this.id = id;
         this.webSiteId = webSiteId;
         this.baseURL = baseURL;
+        this.name = name;
+        this.description = description;
+        this.overlayType = overlayType;        
         this.recordingMode = recordingMode;
     }
 
