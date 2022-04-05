62fd6e70b93f73ebc50ca3842263efbdbca7413a
Xavier Blanc <xavier.blanc@u-bordeaux.fr>
2022-04-05 14:05:07 +0200

    mode research

---
+++ browser-script-research/src/index_old.ts
@@ -0,0 +1,33 @@
+import BrowserScript from "./domain/BrowserScript";
+import {logger} from "./framework/Logger";
+import AifexServiceHTTP from "./_infra/AifexServiceHTTP";
+import BrowserServiceLocalStorage from "./_infra/BrowserServiceLocalStorage";
+import BrowserServiceSessionStorage from "./_infra/BrowserServiceSessionStorage";
+
+logger.info("AIFEX script is running.");
+
+const AIFEX_SCRIPT = document.getElementById("AIFEX");
+if (AIFEX_SCRIPT) {
+    logger.info("AIFEX SCRIPT TAG is found.");
+    const CONNEXION_URL = AIFEX_SCRIPT.getAttribute("connexion-url");
+    if (CONNEXION_URL) {
+        logger.info("AIFEX connexion-url Element is found.");
+        try {
+            const AIFEX_URL = new URL(CONNEXION_URL);
+			let sessionId = AIFEX_URL.searchParams.get('sessionId');
+			if (sessionId) {
+                logger.info("AIFEX sessionId is found.");
+                const AIFEX_SERVICE = new AifexServiceHTTP();
+                const BROWSER_SERVICE = new BrowserServiceSessionStorage();
+                const BROWSER_SCRIPT = new BrowserScript(AIFEX_URL.origin, sessionId, undefined, AIFEX_SERVICE, BROWSER_SERVICE);
+                BROWSER_SCRIPT.start();	
+			}
+        } catch (e) {
+            logger.error("Invalid connexion URL", new Error("Invalid connexion URL"));
+        }
+    }
+} else {
+    logger.error("AIFEX SCRIPT Element is not found.", new Error("AIFEX SCRIPT Element is not found."));
+}
+
+// <script id="AIFEX" connexion-url="https://aifex.com/aifex/connexion?sessionId=a1b2c3d4e5f6g7h8i9j0" src="https://aifex.com/aifex/script/aifex.js"></script>
