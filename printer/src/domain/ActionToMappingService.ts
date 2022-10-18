570f167d89447ff754f6506040b96786bddd2925
Xavier Blanc <xavier.blanc@u-bordeaux.fr>
2022-10-18 11:37:42 +0200

    remove printer ... was legacy code

--- printer/src/domain/ActionToMappingService.ts
+++
@@ -1,9 +0,0 @@
-import Action from "./Action";
-import Mapping from "./Mapping";
-
-// Value Object
-export default class ActionToMappingService {
-    public static findMappingForAction(action: Action, mappingList: Mapping[]): Mapping | undefined{
-        return mappingList.find((mapping) => mapping.output.prefix === action.kind);
-    }
-}
--- printer/src/domain/SessionRepository.ts
+++
@@ -1,5 +0,0 @@
-import Session from "../domain/Session";
-
-export default interface SessionRepository {
-    findSessionById(sessionId: string): Promise<Session | undefined>;
-}
